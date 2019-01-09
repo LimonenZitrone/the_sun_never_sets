9ff9e3b653ec2c47cc5b5a0e1ce39fc51d99187d
LimonenZitrone <Halo270@web.de>
2019-01-09 02:53:20 +0100

    update and fixes

---
+++ common/defines/sns_defines.lua
@@ -0,0 +1,7 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.START_DATE = "1836.1.1"
+NDefines.NGame.END_DATE = "9999.12.31"
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "9999.12.31"
