303697104ef3c4906dc0c87e2e00fc0ee8e6e511
LimonenZitrone <Halo270@web.de>
2020-09-05 01:31:56 +0200

    common backup folder moved

---
+++ _common backup/defines/sns_defines.lua
@@ -0,0 +1,52 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.START_DATE = "1836.1.1"
+NDefines.NGame.END_DATE = "9999.12.31"
+NDefines.NGame.AGE_USHER_IN_TIME = 12
+NDefines.NCountry.GOLDEN_ERA_YEARS = 25
+NDefines.NCountry.PS_FACTION_BOOST = 9999
+
+-- Minimap stuff
+NDefines.NGraphics.MINIMAP_WATER_COLOR_R = 255
+NDefines.NGraphics.MINIMAP_WATER_COLOR_G = 255
+NDefines.NGraphics.MINIMAP_WATER_COLOR_B = 255
+NDefines.NGraphics.MINIMAP_LAND_COLOR_R = 149
+NDefines.NGraphics.MINIMAP_LAND_COLOR_G = 131
+NDefines.NGraphics.MINIMAP_LAND_COLOR_B = 94
+NDefines.NGraphics.MINIMAP_LAND_COLOR_BASE_LERP = 1
+
+--- Map stuff
+NDefines.NFrontend.CAMERA_LOOKAT_X = 2787.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1845.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 2787.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1845.0
+NDefines.NFrontend.CAMERA_START_X = 2787.0
+NDefines.NFrontend.CAMERA_START_Z = 1755.0
+NDefines.NFrontend.CAMERA_END_X = 2787.0
+NDefines.NFrontend.CAMERA_END_Z = 1700.0
+NDefines.NFrontend.FRONTEND_POS_X = 2787.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1825.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2787.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1845.0
+NDefines.NFrontend.SETTINGS_POS_X = 2787.0
+NDefines.NFrontend.SETTINGS_POS_Z = 1228.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2787.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1499.0
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2787.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1398.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2787.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1459.0
+NDefines.NFrontend.TUTORIAL_POS_X = 2724.0
+NDefines.NFrontend.TUTORIAL_POS_Z = 1654.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 2710.0
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1792.0
+NDefines.NFrontend.CONTENT_POS_X = 2787.0
+NDefines.NFrontend.CONTENT_POS_Z = 1642.0
+NDefines.NFrontend.CONTENT_LOOK_X = 2787.0
+NDefines.NFrontend.CONTENT_LOOK_Z = 1845.0
+NDefines.NFrontend.CREDITS_POS_X = 2911.0
+NDefines.NFrontend.CREDITS_POS_Z = 1824.0
+NDefines.NFrontend.CREDITS_LOOK_X = 2916.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 2027.0
